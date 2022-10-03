969425d57fe2dfb9ad7c723bdc03310af48775c7
Magerick <112679606+Magerick@users.noreply.github.com>
2022-10-02 20:42:21 -0700

    Added var userInput and passwordLength. Added an if function and an OR comparator for passwords between 8-128 characters long.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Added var userInput and passwordLength.
+function generatePassword() {
+  var userInput = window.prompt("How many characters do you want in your password?")
+  var passwordLength = parseInt(userInput)
+}
+
+// Added an if function and an OR comparator for passwords between 8-128 characters long.
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("The password must be 8 to 128 characters long.")
+  return;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
