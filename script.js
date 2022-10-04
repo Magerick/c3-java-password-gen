b9f1f1dd2d6bb6d5a0c85170419267856517e7c4
Magerick <112679606+Magerick@users.noreply.github.com>
2022-10-03 17:03:47 -0700

    Made it so if the window prompts are canceled, the password defaults to lowercase letters using an if statement under var optionsCart.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Added var generatedPassword and function randomInt/getRandomInt.
+  // Added an if function and a var rand.
+  function randomInt(min, max) {
+    if (!max); {
+      max = min;
+      min = 0;
+    }
+    var rand = Math.random();
+    return Math.floor(min*(1 - rand) + rand*max);
+  }
+
+  function getRandomItem(list) {
+    return list[randomInt(list.length)];
+  }
+
 // Added var userInput and passwordLength.
 // Adjusted indentation for the whole code chain below function generatePassword().
 function generatePassword() {
@@ -9,7 +24,13 @@ function generatePassword() {
   
   // Added an if function and an OR comparator for passwords between 8-128 characters long.
   if (passwordLength < 8 || passwordLength > 128) {
-   window.alert("The password must be 8 to 128 characters long.");
+    window.alert("The password must be 8 to 128 characters long.");
+    return;
+  }
+
+  // Added an alert box if the user does not input a number.
+  if (isNaN(passwordLength)) {
+    window.alert("Use a number.");
     return;
   }
 
@@ -23,52 +44,46 @@ function generatePassword() {
   // Maded a list for the items above.
   var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseList = [];
+
   // Added a for loop to hopefully convert the lowercase array to UPPERCASE to make my life easier.
-    for (var i = 0; i < lowercaseList.length; i++) {
-      uppercaseList[i] = lowercaseList [i].toUpperCase();
-    }
+  for (var i = 0; i < lowercaseList.length; i++) {
+  uppercaseList[i] = lowercaseList[i].toUpperCase();
+
   var numbersList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   // Added a bunch of symbols that usually work whenever I have to make new passwords. Don't know if ` ; : ' " , or . work (of course periods don't work in passwords).
   var symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "~", "_", "+", "<", ">", "/", "?"];
 
   // Added a cart to link the options above.
   // Added booleans for the cart.
-  var optionsCart = [];
-    if (userWantsLowercase === true) {
-      optionsCart.push(lowercaseList);
-    }
-
-    if (userWantsUppercase === true) {
-      optionsCart.push(uppercaseList);
-    }
-  
-    if (userWantsNumbers === true) {
-      optionsCart.push(numbersList);
-    }
-  
-    if (userWantsSymbols === true) {
-      optionsCart.push(symbolsList);
-    }
-
-  // Added var generatedPassword and function randomInt/getRandomInt.
-  // Added an if function and a var rand.
-  function randomInt(min, max) {
-    if (!max); {
-      max = min;
-      min = 0;
-    }
-    var rand = Math.random();
-    return Math.floor(min*(1-rand)+rand*max);
+  var optionsCart = []; 
   }
 
-  function getRandomItem(list) {
-    return list[randomInt(0, list.length-1)];
+  if (userWantsLowercase === true) {
+    optionsCart.push(lowercaseList);
+  }
+
+  if (userWantsUppercase === true) {
+    optionsCart.push(uppercaseList);
+  }
+  
+  if (userWantsNumbers === true) {
+    optionsCart.push(numbersList);
+  }
+  
+  if (userWantsSymbols === true) {
+    optionsCart.push(symbolsList);
+  }
+
+  // Made it so if no options above are selected, the password defaults to lowercase letters.
+  if (optionsCart.length === 0) {
+    optionsCart.push(lowercaseList);
   }
 
   var generatedPassword = "";
     
   // Fixed indentation and added += randomChar.
-  for (var i = 0; 1 < passwordLength; i++) {
+  // Fixed a typo: 1 should be i.
+  for (var i = 0; i < passwordLength; i++) {
     var randomList = getRandomItem(optionsCart);
     var randomChar = getRandomItem(randomList);
     generatedPassword += randomChar;
@@ -76,6 +91,7 @@ function generatePassword() {
 
   // Added a console log to see if the password generated.
   console.log(generatedPassword);
+
 }
 
 // Write password to the #password input
